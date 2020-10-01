939167d51b6cd2ff4f1efd75dee89e32ef1664a2
Sam DePoule <samdepoule@gmail.com>
2020-09-30 23:26:05 -0700

    Neatening and reviewing/understanding code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/GoalLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/GoalLift.java
@@ -23,8 +23,9 @@ public class GoalLift {
      * Creates a GoalLift with the default name for the motor
      * @param hw robot's hardware map
      */
-    public GoalLift(HardwareMap hw){
-        motor = hw.dcMotor.get("goalLiftMotor");
+    public GoalLift( HardwareMap hw ){
+
+        motor = hw.dcMotor.get( "goalLiftMotor" );
     }
 
     /**
@@ -32,16 +33,18 @@ public class GoalLift {
      * @param hw robot's hardware map
      * @param motorName name of the lift motor in the hardware map
      */
-    public GoalLift(HardwareMap hw, String motorName){
-        motor = hw.dcMotor.get(motorName);
+    public GoalLift( HardwareMap hw, String motorName ) {
+
+        motor = hw.dcMotor.get( motorName );
     }
 
     /**
      * Sets power to the goal lift
      * @param power power at which to run the motor
      */
-    public void setGoalLiftPower(double power){
-        motor.setPower(power);
+    public void setGoalLiftPower( double power ) {
+
+        motor.setPower( power );
     }
 
     /**
@@ -49,19 +52,20 @@ public class GoalLift {
      * @param position position to move the lift to
      * @param power power at which to move the lift
      */
-    public void setGoalLiftPosition(LiftPosition position, double power){
-        switch(position){
+    public void setGoalLiftPosition( LiftPosition position, double power ) {
+
+        switch( position ) {
             case TOP: {
-                if (currentPosition == LiftPosition.TOP) {
+                if ( currentPosition == LiftPosition.TOP )
                     break;
-                }
-                liftToPosition(TICKS_TO_LIFTED_POSITION, power);
+
+                liftToPosition( TICKS_TO_LIFTED_POSITION, power );
             }
             case BOTTOM:{
-                if(currentPosition == LiftPosition.BOTTOM){
+                if( currentPosition == LiftPosition.BOTTOM )
                     break;
-                }
-                liftToPosition(0, power);
+
+                liftToPosition( 0, power );
             }
         }
 
@@ -72,12 +76,13 @@ public class GoalLift {
      * @param position position to move the lift to
      * @param power power at which to move the lift
      */
-    private void liftToPosition(int position, double power){
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor.setTargetPosition(position);
-        setGoalLiftPower(power);
-        while(motor.isBusy());
-        setGoalLiftPower(0);
-        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    private void liftToPosition( int position, double power ) {
+
+        motor.setMode( DcMotor.RunMode.RUN_TO_POSITION );
+        motor.setTargetPosition( position );
+        setGoalLiftPower( power );
+        while( motor.isBusy() );
+            setGoalLiftPower( 0 );
+        motor.setMode( DcMotor.RunMode.RUN_WITHOUT_ENCODER );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/RingShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/RingShooter.java
@@ -19,7 +19,7 @@ public class RingShooter {
      * @param hw robot's hardware map
      */
     public RingShooter(HardwareMap hw){
-        setUpHardware(hw, "intakeMotor", "leftFlyWheelMotor", "rightFlyWheelMotor");
+        setUpHardware( hw, "intakeMotor", "leftFlyWheelMotor", "rightFlyWheelMotor" );
     }
 
     /**
@@ -29,8 +29,8 @@ public class RingShooter {
      * @param leftFlyWheelName name of left flywheel motor in the hardware map
      * @param rightFlyWheelName name of right flywheel motor in the hardware map
      */
-    public RingShooter(HardwareMap hw, String intakeMotorName, String leftFlyWheelName, String rightFlyWheelName){
-        setUpHardware(hw, intakeMotorName, leftFlyWheelName, rightFlyWheelName);
+    public RingShooter( HardwareMap hw, String intakeMotorName, String leftFlyWheelName, String rightFlyWheelName ) {
+        setUpHardware( hw, intakeMotorName, leftFlyWheelName, rightFlyWheelName );
     }
 
     /**
@@ -40,11 +40,12 @@ public class RingShooter {
      * @param leftFlyWheelName name of left flywheel motor in the hardware map
      * @param rightFlyWheelName name of right flywheel motor in the hardware map
      */
-    private void setUpHardware(HardwareMap hw, String intakeMotorName, String leftFlyWheelName, String rightFlyWheelName){
-        intakeMotor = hw.dcMotor.get(intakeMotorName);
+    private void setUpHardware( HardwareMap hw, String intakeMotorName, String leftFlyWheelName, String rightFlyWheelName ) {
 
-        leftFlyWheelMotor = hw.dcMotor.get(leftFlyWheelName);
-        rightFlyWheelMotor = hw.dcMotor.get(rightFlyWheelName);
+        intakeMotor = hw.dcMotor.get( intakeMotorName );
+
+        leftFlyWheelMotor = hw.dcMotor.get( leftFlyWheelName );
+        rightFlyWheelMotor = hw.dcMotor.get( rightFlyWheelName );
     }
 
     /**
@@ -53,6 +54,7 @@ public class RingShooter {
      * @param power power at which to run the intake motor
      */
     public void setIntakeMotorPower( double power ) {
+
         intakeMotor.setPower( power );
     }
 
@@ -61,8 +63,8 @@ public class RingShooter {
      *
      * @param power power at which to run the flywheels
      */
-
     public void setFlyWheelMotorPower( double power) {
+
         //the negative sign depends on how the robot is set up
         leftFlyWheelMotor.setPower( power );
         rightFlyWheelMotor.setPower( -power );
