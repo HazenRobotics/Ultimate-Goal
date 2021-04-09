886885055ac6bcdb8224199bea54136b7b3f870d
corbant <corbant25@gmail.com>
2021-04-09 16:55:04 -0700

    Switching Branches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousTest.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.mechanisms.GoalLift;
 import org.firstinspires.ftc.teamcode.robots.Robot;
+import org.firstinspires.ftc.teamcode.robots.RobotTechnicolorRR;
 import org.firstinspires.ftc.teamcode.robots.RobotWood;
 import org.firstinspires.ftc.teamcode.drives.MecanumDrive;
+import org.firstinspires.ftc.teamcode.utils.SoundLibrary;
+import org.firstinspires.ftc.teamcode.utils.TensorFlowUtil;
+import org.firstinspires.ftc.teamcode.utils.Vuforia;
 
 // autonomous program that drives bot forward a set distance, stops then
 // backs up to the starting point using encoders to measure the distance.
@@ -15,25 +21,41 @@ import org.firstinspires.ftc.teamcode.drives.MecanumDrive;
 //@disabled
 public class AutonomousTest extends LinearOpMode
 {
-    //Robot robot;
-    RobotWood robotWood;
+    RobotTechnicolorRR robot;
+    private TensorFlowUtil.Stack stack;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         Robot.createDefaultMatchLogFileName( this.getClass().getSimpleName() );
 
-        //robot = new RobotWood(hardwareMap, this);
-        //robotWood = new RobotWood(hardwareMap, this);
+        robot = new RobotTechnicolorRR(hardwareMap, this);
 
-        robotWood.tensorFlowUtil.initTensorFlow();
+        SoundLibrary.playStartup();
+
+        robot.setPosition(new Pose2d(-62, -48));
+        robot.goalLift.setClawPosition(GoalLift.ClawPosition.CLOSED);
+        robot.tfod.initTensorFlow();
+
+        telemetry.addLine("Initialization Complete");
+        telemetry.update();
+
+        Robot.writeToMatchDefaultFile( "Init Finished", true );
+
+        while(!isStarted()) {
+            if(isStopRequested()) {
+                robot.tfod.deactivateTensorFlow();
+                if(Vuforia.getInstance().isRunning())
+                    Vuforia.getInstance().close();
+            }
+        }
 
         waitForStart();
 
         //==========================================================================================
         //Official Start
 
-        robotWood.tensorFlowUtil.runStackDetection(20000);
+        robot.tfod.runStackDetection(20000);
 
 
 
