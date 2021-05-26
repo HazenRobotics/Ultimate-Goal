80d0bdf10758ac7f224d369cd81c79336b91a8ae
Sam DePoule <samdepoule@gmail.com>
2021-05-26 14:53:25 -0700

    Added a Tutorial TeleOp class for newcomers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TutorialTeleOp.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// this is so that this class will display in the Driver Station app so that we can run it
+@TeleOp(name = "TutorialTeleOp", group = "teleop")
+public class TutorialTeleOp extends OpMode {
+    
+    
+    @Override
+    public void init( ) { // this will get any devices (i.e. motors, sensors, gyros, etc. )
+    
+    
+    }
+    
+    @Override
+    public void loop( ) { // this method will continuously loop while the program is running or the OpMode is active
+    
+    
+    }
+    
+    
+}
